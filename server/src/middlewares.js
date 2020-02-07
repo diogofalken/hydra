ed1a9afc4da4593e5e8f6cfa6cb90e12de30151c
diogofalken <diogofalken0@gmail.com>
2020-02-07 14:25:03 +0000

    :books: Documenting Code using JSDoc

--- server/src/middlewares.js
+++ server/src/middlewares.js
@@ -1,9 +1,30 @@
+/**
+ * Middlewares modules - Has all the custom middlewares that will be used by Express
+ * @module middlewares
+ */
+
+/**
+ * Method responsible when a route was not found
+ * @param {Express.Request} req - Incoming information about Request
+ * @param {Express.Response} res - Incoming information about Response
+ * @param {Function} next - Function that when called passes to next Middleware
+ * @returns {void}
+ */
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
+/**
+ * Method responsible when a route was not found
+ * @param {string} error - Error, passed by notFound function
+ * @param {Express.Request} req - Incoming information about Request
+ * @param {Express.Response} res - Incoming information about Response
+ * @param {Function} next - Function that when called passes to next Middleware
+ * @returns {Object} - Returns a response with the error message and the error stack if we are
+ * in the development mode
+ */
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (error, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,40 @@
+/** Importing NodeJS modules */
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+/** Middleware import */
 const middlewares = require('./middlewares');
 
+/**
+ * @file server.js is the root file for this API
+ * @author Diogo Costa
+ * @see <a href="https://diogomarques.tk">Diogo Costa</a>
+ */
+
+/** Creates an Express Application */
 const app = express();
+
+/**
+ * HTTP request logger middleware for node.js
+ * @see <a href="https://www.npmjs.com/package/morgan">Morgan npm</a>
+ */
+/** HTTP request logger middleware for nodejs */
 app.use(morgan('common'));
+
+/**
+ * Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver
+ * bullet but, it can help!
+ * @see <a href="https://www.npmjs.com/package/helmet">Helmet npm</a>
+ */
 app.use(helmet());
+
+/**
+ * CORS is a node.js package for providing a Connect/Express middleware that can be used to
+ * enable CORS with various options.
+ * @see <a href="https://www.npmjs.com/package/cors">Cors npm</a>
+ */
 app.use(
   cors({
     origin: 'http://localhost:3000',
@@ -20,10 +47,23 @@ app.get('/', (req, res) => {
   });
 });
 
+/**
+ * Not Found is responsible for when a route is not found
+ * @see {@link middlewares}
+ */
 app.use(middlewares.notFound);
 
+/**
+ * Error Handler is the last middleware to be invoked since it will take care of all
+ * the errors and will be catched in this function
+ * @see {@link middlewares}
+ */
 app.use(middlewares.errorHandler);
 
+/**
+ * Port that API will listen
+ * @type {number}
+ */
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}...`);
