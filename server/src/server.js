a8636269cf9bb110ba2c5cdc094410653329808b
diogofalken <diogofalken0@gmail.com>
2020-02-07 00:57:06 +0000

    ✨ API Setup and not found and error handler middleware done

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Ma Naem Jeff!',
+    message: '🚀',
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: error.stack === 'production' ? '🍑' : error.stack,
   });
 });
 
